0a14a41f796834f0d81aafb2e772216e4ead82ef
Mia Moir <me@archess.mn>
2025-06-20 16:35:07 +0100

    Use consul for the backend
    Saves me having to push state to git

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "consul" {
+    address = "consul.service.consul:8500"
+    scheme = "http"
+    path = "terraform/rosesmedia/dns"
+    lock = true
+  }
+  
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
